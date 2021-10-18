ba18697fe4735e2a6dbc3a13fb7addea04e0f4ca
EvanZ <2023ezhang3@tjhsst.edu>
2021-10-18 15:58:47 -0400

    V2

--- app/src/main/java/com/example/drawings/DrawView.java
+++ app/src/main/java/com/example/drawings/DrawView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,8 +22,15 @@ public class DrawView extends View { //10:19
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         p.setColor(Color.MAGENTA);
-        canvas.drawCircle(100, y, (float) 105,  p);
-        canvas.drawCircle(400, 200, (float) 105,  new Paint());
+        int random = (int) Math.random() * 3;
+        if (random == 0) {
+            canvas.drawLine(0, 0, 10, 5, p);
+        } else if (random == 1) {
+            canvas.drawRect(new Rect(0, 0, 10, 10), p);
+        } else {
+            canvas.drawCircle(0, 0, 10f, p);
+        }
+        //canvas.drawLine(0f, (float) y, 10f, (float) (y + 10));
         y+=dY;
         if (y <= 0) {
             y = getHeight() - 1;
--- app/src/main/java/com/example/drawings/MainActivity.java
+++ app/src/main/java/com/example/drawings/MainActivity.java
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     DrawView drawView;
+    TextView textView;
+    FragmentA fragmentA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawView = findViewById(R.id.drawView);
+        textView = findViewById(R.id.main_textview);
+        fragmentA = (FragmentA)getSupportFragmentManager().findFragmentById(R.id.a_fragment);
     }
 
     public void reverseY(View view) {
         drawView.setdY(drawView.getdY()*-1);
+    }
+
+    public void update_main(View view) {
+        textView.setText("Hello TJHSST");
+        ((Button)view).setText("I have been clicked");
+        fragmentA.update("I have been updated");
     }
 }
